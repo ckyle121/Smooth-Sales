1a6a396e4f4cddd819d571cc17147ca740e86841
ckyle121 <carolinekyle21@gmail.com>
2022-03-07 17:02:15 -0600

    add product crud api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{ model: Category }, { model: Tag }]
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+        id: req.params.id
+    },
+    include: [{ model: Category }, { model: Tag }]
+  })
+  .then(dbPostData => {
+    if (!dbPostData){
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -84,13 +105,28 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbPostData => {
+    if (!dbPostData){
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 module.exports = router;
